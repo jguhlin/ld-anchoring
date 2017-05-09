9c7ddd69b0861122b48852e635df21de4d8707ad
jguhlin <joseph.guhlin@gmail.com>
2017-05-09 15:43:28 -0500

    Updates

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject ld-anchoring "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :plugins [[lein-gorilla "0.4.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
